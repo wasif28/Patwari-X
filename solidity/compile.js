204855335a00379a1756c74738d44bf2233c7129
wasif28 <wasif28@hotmail.com>
2019-10-03 02:26:05 +0500

    Add files via upload

---
+++ solidity/compile.js
@@ -0,0 +1,9 @@
+const path= require('path');
+const fs=  require('fs');
+const solc= require('solc');
+
+const patwarixPath= path.resolve(__dirname, 'contracts', 'patwarix.sol');
+const source = fs.readFileSync(patwarixPath, 'utf8');
+
+//console.log(solc.compile(source, 1).contracts[':Property']);
+module.exports = solc.compile(source, 1).contracts[':Property'];
