74a068cf40b445336940b5c77e9e15920de5c2f7
Yasir <yasir26563@gmail.com>
2020-02-22 21:14:20 +0500

    FrontEnd Update

---
+++ FrontEnd/Light/src/app/bid/bid.component.ts
@@ -0,0 +1,17 @@
+/*
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './bid.component.html',
+  styleUrls: ['./bid.component.css']
+})
+export class UserComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
+*/
---
+++ FrontEnd/Light/src/app/notifications/notifications.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+
+declare var $:any;
+
+@Component({
+  selector: 'app-notifications',
+  templateUrl: './notifications.component.html',
+  styleUrls: ['./notifications.component.css']
+})
+export class NotificationsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+  showNotification(from, align){
+      const type = ['','info','success','warning','danger'];
+
+      var color = Math.floor((Math.random() * 4) + 1);
+      $.notify({
+          icon: "pe-7s-gift",
+          message: "PatwariX."
+      },{
+          type: type[color],
+          timer: 1000,
+          placement: {
+              from: from,
+              align: align
+          }
+      });
+  }
+}
